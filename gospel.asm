b4e6412f0a3ba6e2369ffbaffeaf197d8a0cdbb4
YanirValdman <tambalnir@gmail.com>
2026-04-26 12:10:17 +0300

    God's new word
    God has commanded me to change the amount of words from 5 to 10

--- gospel.asm
+++ gospel.asm
@@ -15,6 +15,21 @@ len4 equ $ - word4
 word5 db "purpose", 10
 len5 equ $ - word5
 
+word6 db "holy", 10
+len6 equ $ - word6
+
+word7 db "love", 10
+len7 equ $ - word7
+
+word8 db "believe", 10
+len8 equ $ - word8
+
+word9 db "Temple", 10
+len9 equ $ - word9
+
+word10 db "Birth", 10
+len10 equ $ - word10
+
 urandom db "/dev/urandom", 0
 
 section .bss
@@ -43,13 +58,20 @@ read_input:
     syscall
 
     mov rdi, rax
+
     mov rax, 0
     mov rsi, buffer
     mov rdx, 1
     syscall
 
-    mov al, [buffer]
-    and al, 7
+    mov rax, 3
+    syscall
+
+    movzx rax, byte [buffer]
+    xor rdx, rdx
+    mov rcx, 10
+    div rcx
+    mov al, dl
 
     cmp al, 0
     je print1
@@ -59,7 +81,18 @@ read_input:
     je print3
     cmp al, 3
     je print4
-    jmp print5
+    cmp al, 4
+    je print5
+    cmp al, 5
+    je print6
+    cmp al, 6
+    je print7
+    cmp al, 7
+    je print8
+    cmp al, 8
+    je print9
+    cmp al, 9
+    je print10
 
 print1:
     mov rsi, word1
@@ -84,10 +117,35 @@ print4:
 print5:
     mov rsi, word5
     mov rdx, len5
+    jmp write
+
+print6:
+    mov rsi, word6
+    mov rdx, len6
+    jmp write
+
+print7:
+    mov rsi, word7
+    mov rdx, len7
+    jmp write
+
+print8:
+    mov rsi, word8
+    mov rdx, len8
+    jmp write
+
+print9:
+    mov rsi, word9
+    mov rdx, len9
+    jmp write
+
+print10:
+    mov rsi, word10
+    mov rdx, len10
 
 write:
-    mov rax, 1
-    mov rdi, 1
+    mov rax, 1    
+    mov rdi, 1      
     syscall
 
     jmp main_loop
